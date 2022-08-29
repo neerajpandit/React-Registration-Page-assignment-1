e5b8370ce5c41fecbeeb0633f9db75e01e655f24
Neerajpandey1507 <pandeyneeraj1507@gmail.com>
2022-08-29 15:33:27 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+//import { useForm } from 'react-hook-form';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //<div className="App">
+      <div className='container'>
+      <form>
+        <div className="register">
+            <div className="col-1">
+                <h2>Certificate of Appreciation</h2>
+            <form id='form' className="flex flex-col">
+                    <input type="text" placeholder='name'/>
+                    <input type="text" placeholder='email'/>
+                    <input type="text" placeholder='session type example:workshop'/>
+                    <input type="text" placeholder='title'/>
+                    <h3>date</h3>
+                    <input type="date" placeholder='date'/>
+                    <button className="btn">Send certificate email</button>
+                </form>
+            </div>
+        </div>
+
+        </form>
+    </div>    
+   // </div>
   );
 }
 
